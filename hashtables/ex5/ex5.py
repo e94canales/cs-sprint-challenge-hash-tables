af6e2e8994a6c3d380301f6281aaf24096cc1aa7
Erick Canales <e94canales@gmail.com>
2020-08-07 11:33:06 -0700

    MVP/STRECTH

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,5 +1,6 @@
 # Your code here
 
+
 def finder(files, queries):
     """
     YOUR CODE HERE
@@ -9,27 +10,45 @@ def finder(files, queries):
     result = []
 
     for string in files:
-        words = string[1:].split("/")
-        for word in words:
-            if word not in cache:
-                cache[word] = []
-            
+        word = string.split("/")[-1]
+
+        if word not in cache and "nofile" not in word:
+            cache[word] = [string]
+        elif word in cache:
             cache[word].append(string)
 
-    result = [paths[1] for paths in cache.items() if paths[0] in queries]
 
-    return result
+    for q in queries:
+        if q in cache:
+            result.append(cache[q])
+
+    flattenedResults = [finalRes for arr in result for finalRes in arr]
+    return flattenedResults
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
+    files = []
+
+    for i in range(500000):
+        files.append(f"/dir{i}/file{i}")
+
+    for i in range(500000):
+        files.append(f"/dir{i}/dirb{i}/file{i}")
+
+    queries = []
+
+    for i in range(1000000):
+        queries.append(f"nofile{i}")
+
+    queries += [
+        "file3490",
+        "file256",
+        "file999999",
+        "file8192"
     ]
     print(finder(files, queries))
+
+    # ['/dir256/dirb256/file256',
+    #         '/dir256/file256', '/dir3490/dirb3490/file3490',
+    #         '/dir3490/file3490', '/dir8192/dirb8192/file8192',
+    #         '/dir8192/file8192']
